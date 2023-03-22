151ed6438cd272bd94c2b5f3550a4790c0088016
j1sk1ss <fotikolay222@gmail.com>
2023-03-22 14:24:32 +0500

    refactored forward feed start creating back prop method

--- FotNET/NETWORK/LAYERS/RECURRENT/RecurrentLayer.cs
+++ FotNET/NETWORK/LAYERS/RECURRENT/RecurrentLayer.cs
@@ -7,24 +7,19 @@ public class RecurrentLayer : ILayer {
 
     public RecurrentLayer(Function function, int size) {
         InputWeights  = new Matrix(1, size);
-        
         HiddenWeights = new Matrix(size, size);
-        HiddenBias = new double[size];
-        
         OutputWeights = new Matrix(size, 1);
-        OutputBias = new double[1];
+
+        HiddenBias = new double[size];
+        HiddenBias[0] = .01d;
+        
+        OutputBias = new double[size];
+        for (var i = 0; i < size; i++) {
+            HiddenBias[i] = .01d;
+        }
         
         Function = function;
 
-        HiddenNeurons = new double[size];
-
-        for (var i = 0; i < size; i++) {
-            HiddenNeurons[i] = .01;
-            HiddenBias[i] = .01;
-        }
-        
-        OutputNeurons = new double[size];
-        
         InputWeights.HeInitialization();
         HiddenWeights.HeInitialization();
         OutputWeights.HeInitialization();
@@ -39,43 +34,53 @@ public class RecurrentLayer : ILayer {
     private Matrix OutputWeights { get; }
     
     private double[] OutputBias { get; }
-    
+
     private Function Function { get; }
     
-    public double[] HiddenNeurons { get; }
-    public double[] OutputNeurons { get; set; }
+    
+    private List<double[]> HiddenNeurons { get; set; }
+    private List<double> OutputNeurons { get; set; }
+    
 
-    public Tensor GetNextLayer(Tensor tensor) =>
-        new Vector(new RecurrentLayer(Function, 1).ForwardFeed(tensor, HiddenNeurons).OutputNeurons).AsTensor(1, OutputNeurons.Length, 0);
-
-    private RecurrentLayer ForwardFeed(Tensor tensor, double[] hiddenNeurons) {
+    public Tensor GetNextLayer(Tensor tensor) {
         var sequence = tensor.Flatten();
-        var tensorSequence = new List<double>();
-        var currentElement = 0d;
-        
-        for (var i = 0; i < sequence.Count; i++) {
-            if (i == 0) {
-                currentElement = sequence[i];
-                continue;
-            }
+        var output = new List<double>();
+        var hidden = new List<double[]>();
 
-            tensorSequence.Add(sequence[i]);
+        for (var step = 0; step < sequence.Count; step++) {
+            var currentElement = sequence[step];
+            var input = (InputWeights * currentElement).GetAsList().ToArray();
+            
+            if (step > 0)
+                hidden.Add(new Vector(new Vector(input) 
+                                      + new Vector(hidden[step - 1] * HiddenWeights)) + new Vector(HiddenBias));
+            else
+                hidden.Add(input);
+
+            hidden[^1] = Function.Activate(new Vector(hidden[^1])
+                .AsTensor(1, hidden[^1].Length, 0)).Flatten().ToArray();
+            output.Add((new Vector(hidden[^1] * OutputWeights) + new Vector(OutputBias))[0]);
         }
 
-        var nextStepTensor = new Vector(tensorSequence.ToArray()).AsTensor(1, tensorSequence.Count, 0);
+        HiddenNeurons = hidden;
+        OutputNeurons = output;
         
-        var neurons = (InputWeights * currentElement).GetAsList().ToArray();
-        var currentHiddenNeurons = Function.Activate(new Vector(new Vector(hiddenNeurons * HiddenWeights) + new Vector(neurons))
-            .AsTensor(1, neurons.Length, 0)).Flatten().ToArray();
-        
-        OutputNeurons = currentHiddenNeurons * OutputWeights;
-        
-        return new RecurrentLayer(Function, 1).ForwardFeed(nextStepTensor, HiddenNeurons);
+        return new Vector(output.ToArray()).AsTensor(1, output.Count, 0);
     }
     
-    public Tensor BackPropagate(Tensor error, double learningRate)
-    {
-        throw new NotImplementedException();
+    public Tensor BackPropagate(Tensor error, double learningRate) {
+        var sequence = error.Flatten();
+        
+        for (var i = sequence.Count - 1; i >= 0; i--) {
+            var currentError = sequence[i];
+            var outputGradient = HiddenNeurons[i]
+        }
+        /*
+         * l1_grad = loss_grad[1].reshape(1,1)
+
+o_weight_grad += hiddens[1][:,np.newaxis] @ l1_grad
+o_bias_grad += np.mean(l1_grad)
+         */
     }
 
     public Tensor GetValues()
