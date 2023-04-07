c0d62a54bb2ab9bd96fccb9fb28ee0ab43099ff4
j1sk1ss <fotikolay222@gmail.com>
2023-04-08 00:05:09 +0500

    tokenizer added

---
+++ FotNET/SCRIPTS/TEXT_CLASSIFICATION/SCRIPTS/Tokenizer.cs
@@ -0,0 +1,33 @@
+﻿namespace FotNET.SCRIPTS.TEXT_CLASSIFICATION.SCRIPTS;
+
+public class Tokenizer {
+    public static List<string> Tokenize(string text) {
+        var whiteSpaces = text.Split(new []{' ', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+        var punctuation = new List<string> { ".", ",", "/", "'", "!", "?", "`", "-", ":"};
+
+        var tokens = new List<string>();
+        foreach (var element in whiteSpaces) {
+            if (punctuation.Contains(element)) continue;
+            
+            var word = "";
+            for (var i = 0; i < element.Length; i++) 
+                if (!punctuation.Contains(element[i] + "")) word += element[i] + "";
+                else if (punctuation.Contains(element[i] + "") && (i == element.Length - 1 || i == 0))
+                    switch (i == 0) {
+                        case true:
+                            tokens.Add(element[i] + "");
+                            break;
+                        default:
+                            tokens.Add(word);
+                            word = "";
+                            tokens.Add(element[i] + "");
+                            break;
+                    }
+            
+            if (word == "") continue;
+            tokens.Add(word);
+        }
+        
+        return tokens;
+    }
+}
---
+++ UnitTests/TokenizerTests.cs
@@ -0,0 +1,15 @@
+﻿using FotNET.SCRIPTS.TEXT_CLASSIFICATION.SCRIPTS;
+
+namespace UnitTests;
+
+public class TokenizerTests {
+    [Test]
+    public void TokenTest() {
+        var text = "Hello, i`m from Netherlands!" +
+                   "\nWhere are u living, dude?";
+        var tokens = Tokenizer.Tokenize(text);
+        foreach (var word in tokens) {
+            Console.WriteLine(word);
+        }
+    }
+}
