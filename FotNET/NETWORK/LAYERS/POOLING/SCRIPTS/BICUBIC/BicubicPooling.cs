167404be22c57a1799f1714e6fbba019ec2f7739
j1sk1ss <fotikolay222@gmail.com>
2023-04-22 14:25:57 +0500

    bicubic pooling

---
+++ FotNET/NETWORK/LAYERS/POOLING/SCRIPTS/BICUBIC/BicubicPooling.cs
@@ -0,0 +1,109 @@
+﻿using FotNET.NETWORK.MATH.OBJECTS;
+
+namespace FotNET.NETWORK.LAYERS.POOLING.SCRIPTS.BICUBIC;
+
+/// <summary>
+/// Takes bicubic value from area and puts in new matrix
+/// </summary>
+public class BicubicPooling : Pooling {
+    protected override Matrix BackPool(Matrix matrix, Matrix referenceMatrix, int poolSize) {
+        var inputWidth  = matrix.Columns;
+        var inputHeight = matrix.Rows;
+
+        var xScale = inputWidth / (inputWidth / (double)poolSize);
+        var yScale = inputHeight / (inputHeight / (double)poolSize);
+
+        var output = new Matrix(inputWidth / poolSize, inputHeight / poolSize);
+
+        for (var y = 0; y < output.Columns; y++) {
+            for (var x = 0; x < output.Rows; x++) {
+                var secondX = x * xScale;
+                var secondY = y * yScale;
+
+                var floorX = (int)Math.Floor(secondX - 1);
+                var floorY = (int)Math.Floor(secondY - 1);
+
+                var samples = new Matrix(4, 4);
+
+                for (var j = 0; j < 4; j++) 
+                    for (var i = 0; i < 4; i++) {
+                        var neighborX = floorX + i;
+                        var neighborY = floorY + j;
+
+                        if (neighborX < 0 || neighborX >= inputWidth || neighborY < 0 || neighborY >= inputHeight)
+                            samples.Body[j, i] = 0;
+                        else
+                            samples.Body[j, i] = matrix.Body[neighborY, neighborX];
+                    }
+                
+                var fractionalX = secondX - floorX - 1;
+                var fractionalY = secondY - floorY - 1;
+
+                var weights = new Matrix(4, 4);
+
+                for (var j = 0; j < 4; j++)
+                    for (var i = 0; i < 4; i++)
+                        weights.Body[j, i] = CubicWeight(i - fractionalX) * CubicWeight(j - fractionalY);
+                
+                double sum = 0;
+                for (var j = 0; j < 4; j++)
+                    for (var i = 0; i < 4; i++)
+                            sum += samples.Body[j, i] * weights.Body[j, i];
+                
+                output.Body[y, x] = sum;
+            }
+        }
+
+        return output;
+    }
+
+    private static double CubicWeight(double value) {
+        const double a = -0.5;
+
+        return Math.Abs(value) switch {
+            >= 2 => 0,
+            >= 1 => ((a + 2) * Math.Abs(value) - (a + 3)) * Math.Pow(Math.Abs(value), 2) + 1,
+            _ => (a + 2) * Math.Pow(Math.Abs(value), 3) - (a + 3) * Math.Pow(Math.Abs(value), 2) + 1
+        };
+    }
+    
+    protected override Matrix Pool(Matrix matrix, int poolSize) {
+        var inHeight = matrix.Rows / poolSize;
+        var inWidth = matrix.Columns / poolSize;
+        var output = new Matrix(inHeight * poolSize, inWidth * poolSize);
+
+        for (var i = 0; i < inHeight; i++) {
+            for (var j = 0; j < inWidth; j++) {
+                for (var k = 0; k < poolSize; k++) {
+                    for (var l = 0; l < poolSize; l++) {
+                        var row = i * poolSize + k;
+                        var col = j * poolSize + l;
+
+                        var xCoordinate = j / (double)inWidth;
+                        var yCoordinate = i / (double)inHeight;
+
+                        var xWeight = 0.0;
+                        var yWeight = 0.0;
+                        for (var m = -1; m <= 2; m++) {
+                            var h = CubicSpline(Math.Abs(m) + (xCoordinate - j) * poolSize);
+                            
+                            xWeight += h * (m < 0 ? -1 : 1) * matrix.Body[row, col + m * poolSize];
+                            h = CubicSpline(Math.Abs(m) + (yCoordinate - i) * poolSize);
+                            yWeight += h * (m < 0 ? -1 : 1) * matrix.Body[row, col + m * poolSize * inWidth];
+                        }
+
+                        output.Body[row, col] = xWeight * yWeight;
+                    }
+                }
+            }
+        }
+
+        return output;
+    }
+    
+    private static double CubicSpline(double x) => Math.Abs(x) switch {
+            <= 1d => 1d - 2d * Math.Pow(Math.Abs(x), 2) + Math.Pow(Math.Abs(x), 3),
+            < 2d => Math.Pow(2d - Math.Abs(x), 3),
+            _ => 0d
+        };
+}
--- FotNET/NETWORK/LAYERS/UP_SAMPLING/UP_SAMPLING_TYPE/BICUBIC_INTERPOLATE/BicubicInterpolate.cs
+++ FotNET/NETWORK/LAYERS/UP_SAMPLING/UP_SAMPLING_TYPE/BICUBIC_INTERPOLATE/BicubicInterpolate.cs
@@ -1,4 +1,5 @@
-﻿using FotNET.NETWORK.MATH.OBJECTS;
+﻿using FotNET.NETWORK.LAYERS.POOLING.SCRIPTS.BICUBIC;
+using FotNET.NETWORK.MATH.OBJECTS;
 
 namespace FotNET.NETWORK.LAYERS.UP_SAMPLING.UP_SAMPLING_TYPE.BICUBIC_INTERPOLATE;
 
@@ -14,52 +15,50 @@ public class BicubicInterpolate : UpSampling {
         for (var y = 0; y < matrix.Columns; y++) {
             for (var x = 0; x < newWidth; x++) {
                 var xScaled = (double)x / scale;
-                var x0 = (int)Math.Floor(xScaled) - 1;
-                var x1 = x0 + 1;
-                var x2 = x0 + 2;
-                var x3 = x0 + 3;
+                var floorX = (int)Math.Floor(xScaled) - 1;
+                var secondX = floorX + 1;
+                var thirdX = floorX + 2;
+                var fourthX = floorX + 3;
                 
-                x0 = Math.Max(0, Math.Min(x0, matrix.Rows - 1));
-                x1 = Math.Max(0, Math.Min(x1, matrix.Rows - 1));
-                x2 = Math.Max(0, Math.Min(x2, matrix.Rows - 1));
-                x3 = Math.Max(0, Math.Min(x3, matrix.Rows - 1));
+                floorX = Math.Max(0, Math.Min(floorX, matrix.Rows - 1));
+                secondX = Math.Max(0, Math.Min(secondX, matrix.Rows - 1));
+                thirdX = Math.Max(0, Math.Min(thirdX, matrix.Rows - 1));
+                fourthX = Math.Max(0, Math.Min(fourthX, matrix.Rows - 1));
 
-                result.Body[x, y] = BicubicInterpolateValue(matrix.Body[x0, y], matrix.Body[x1, y], 
-                    matrix.Body[x2, y], matrix.Body[x3, y], xScaled - x0);
+                result.Body[x, y] = BicubicInterpolateValue(matrix.Body[floorX, y], matrix.Body[secondX, y], 
+                    matrix.Body[thirdX, y], matrix.Body[fourthX, y], xScaled - floorX);
             }
         }
 
         for (var x = 0; x < newWidth; x++) {
             for (var y = 0; y < newHeight; y++) {
                 var yScaled = (double)y / scale;
-                var y0 = (int)Math.Floor(yScaled) - 1;
-                var y1 = y0 + 1;
-                var y2 = y0 + 2;
-                var y3 = y0 + 3;
+                var floorY = (int)Math.Floor(yScaled) - 1;
+                var secondY = floorY + 1;
+                var thirdY = floorY + 2;
+                var fourthY = floorY + 3;
                 
-                y0 = Math.Max(0, Math.Min(y0, matrix.Columns - 1));
-                y1 = Math.Max(0, Math.Min(y1, matrix.Columns - 1));
-                y2 = Math.Max(0, Math.Min(y2, matrix.Columns - 1));
-                y3 = Math.Max(0, Math.Min(y3, matrix.Columns - 1));
+                floorY = Math.Max(0, Math.Min(floorY, matrix.Columns - 1));
+                secondY = Math.Max(0, Math.Min(secondY, matrix.Columns - 1));
+                thirdY = Math.Max(0, Math.Min(thirdY, matrix.Columns - 1));
+                fourthY = Math.Max(0, Math.Min(fourthY, matrix.Columns - 1));
                 
-                result.Body[x, y] = BicubicInterpolateValue(result.Body[x, y0], result.Body[x, y1], 
-                    result.Body[x, y2], result.Body[x, y3], yScaled - y0);
+                result.Body[x, y] = BicubicInterpolateValue(result.Body[x, floorY], result.Body[x, secondY], 
+                    result.Body[x, thirdY], result.Body[x, fourthY], yScaled - floorY);
             }
         }
 
         return result;
     }
     
-    private static double BicubicInterpolateValue(double p0, double p1, double p2, double p3, double x) {
-        var a0 = -0.5*p0 + 1.5*p1 - 1.5*p2 + 0.5*p3;
-        var a1 = p0 - 2.5*p1 + 2*p2 - 0.5*p3;
-        var a2 = -0.5*p0 + 0.5*p2;
+    private static double BicubicInterpolateValue(double firstValue, double secondValue, double thirdValue, double fourthValue, double x) {
+        var a0 = -.5d * firstValue + 1.5d * secondValue - 1.5d * thirdValue + .5d *fourthValue;
+        var a1 = firstValue - 2.5d * secondValue + 2d * thirdValue - .5d * fourthValue;
+        var a2 = -.5d * firstValue + .5d * thirdValue;
         
-        return a0*Math.Pow(x,3) + a1*Math.Pow(x,2) + a2*x + p1;
+        return a0*Math.Pow(x,3) + a1*Math.Pow(x,2) + a2*x + secondValue;
     }
-    
-    protected override Matrix DownSample(Matrix matrix, int scale)
-    {
-        throw new NotImplementedException();
-    }
+
+    protected override Matrix DownSample(Matrix matrix, int scale) =>
+        new BicubicPooling().Pool(new Tensor(matrix), scale).Channels[0];
 }
